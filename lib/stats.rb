16489bc5c6604c8de9a9c9c68fae76f568296e0d
Kaitlyn Tierney <k@krtierney.com>
2020-06-13 19:07:00 +0100

    Sort the log entries by most visited
    This is a bit hard to read, but hopefully the test coverage will make it more obvious what visits_by_page is doing. It strips out the IP source data, since we only want to capture the aggregate number of visits for each page for this stat.

--- lib/stats.rb
+++ lib/stats.rb
@@ -2,7 +2,9 @@
 
 module Stats
   def visits_by_page(log)
-    # p log
+    log.each_with_object({}) do |(page, _v), h|
+      h[page] = log[page].inject(0) { |i, (_k, value)| i + value }
+    end.sort_by(&:last)
   end
 
   def uniques_by_page
--- test/stats_test.rb
+++ test/stats_test.rb
@@ -3,4 +3,19 @@
 require_relative 'spec_helper'
 
 describe Stats do
+  before do
+    @logs = Parser.new(File.new('./test/test.log')).logged_visits
+    @formatter = Formatter.new(@logs)
+  end
+
+  describe '#visits_by_page' do
+    it 'returns a 2d array of visit counts per path' do
+      _(@formatter.visits_by_page(@logs)).must_be_instance_of Array
+    end
+
+    it 'matches expected output for the test log' do
+      expected_values = [['/about', 2], ['/contact', 3], ['/home', 3], ['/help_page/1', 5], ['/index', 6]]
+      assert_equal(@formatter.visits_by_page(@logs), expected_values)
+    end
+  end
 end
